9da654af005afadd451628213a531cc863eb1f72
Vukašin Jekić <vuk.jekic@gmail.com>
2019-05-25 06:40:03 +0200

    tower script & scene init

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float health;
+
+    public enum TowerType {ATTACK, DEFENCE};
+
+    public TowerType towerType;
+    
+
+
+    void Start()
+    {
+        
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
